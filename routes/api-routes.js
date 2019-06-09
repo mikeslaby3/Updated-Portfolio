4576d00f9dc20639c95231fa022a48e75ac8c5e9
Mike Slaby <mikeslaby3@gmail.com>
2019-06-09 00:47:40 -0700

    Created backend post route for contact form

---
+++ routes/api-routes.js
@@ -0,0 +1,20 @@
+const db = require("../models");
+
+module.exports = function(app) {
+
+    app.post("/api/contact", function (req, res) {
+        console.log(req.body);
+    
+        db.Contact.create({
+            name: req.body.name,
+            last_name: req.body.last_name,
+            email: req.body.email,
+            subject: req.body.subject,
+            message: req.body.message,
+        }).then(function (dbContact) {
+            res.json(dbContact);
+        });
+    
+    });
+
+}
