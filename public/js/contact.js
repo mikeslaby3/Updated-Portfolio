ea9f5a509f853259c49287f6b9f8c4ac0d051b5d
Mike Slaby <mikeslaby3@gmail.com>
2019-06-10 17:00:36 -0700

    Included form validation with error and confirmation messages

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,24 +1,52 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    
+    setVisibility('#form-error', 'hide');
+    setVisibility('#form-confirmation', 'hide');
 
-    let contactName = $('#name');
-    let contactLastName = $('#last_name');
-    let contactEmail = $('#email');
-    let contactSubject = $('#subject');
-    let contactMessage = $('#message');
+    $(document).on("click", '#contact-submit', handleContactInput);
 
-    $(document).on("click", '#contact-submit', insertContactInfo);
-
-    function insertContactInfo(event) {
-        event.preventDefault();
-        let contact = {
-            name: contactName,
-            last_name: contactLastName,
-            email: contactEmail,
-            subject: contactSubject,
-            message: contactMessage
-        };
-
-        // $.post("api/contact", contact);
+    function setVisibility(id, showOrHide) {
+        if (showOrHide === 'show') {
+            $(id).show();
+        } else if (showOrHide === 'hide') {
+            $(id).hide();
+        } else {
+            console.log('setVisibility: second arg must be show or hide');
+        }
     }
+
+    function handleContactInput(event) {
+        event.preventDefault();
+
+        let contactName = $('#name').val().trim();
+        let contactLastName = $('#last_name').val().trim();
+        let contactEmail = $('#email').val().trim();
+        let contactSubject = $('#subject').val().trim();
+        let contactMessage = $('#message').val().trim();
+
+        if (contactName === '' ||
+            contactLastName === '' ||
+            contactEmail === '' ||
+            contactSubject === '' ||
+            contactMessage === '') {
+                setVisibility('#form-error', 'show');
+            } else {
+                setVisibility('#form-error', 'hide');
+                let contact = {
+                    name: contactName,
+                    last_name: contactLastName,
+                    email: contactEmail,
+                    subject: contactSubject,
+                    message: contactMessage
+                };
+    
+                $.post("/api/contact", contact)
+                    .then(function(contact) {
+                        $('#contact-form').trigger('reset');
+                        setVisibility('#form-confirmation', 'show');
+                    });
+            }
+
+    };
 
 });
