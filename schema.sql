9e296b484e0d39e0feacc3c490667e5e9a861a0d
Mike Slaby <mikeslaby3@gmail.com>
2019-06-13 21:06:10 -0700

    Got rid of drop db if exists

--- schema.sql
+++ schema.sql
@@ -1,4 +1,1 @@
--- comment out this line once app is ready
--- DROP DATABASE IF EXISTS portfolio_contact;
-
 CREATE DATABASE portfolio_contact;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 8050;
 const app = express();
 
 const db = require('./models');
+const nodemailer = require('nodemailer');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
@@ -12,8 +13,8 @@ app.use(express.json());
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
-db.sequelize.sync({ force: true }).then(function() {
-    app.listen(PORT, function() {
+db.sequelize.sync({ force: true }).then(function () {
+    app.listen(PORT, function () {
         console.log('App listening on PORT ' + PORT);
     });
 });
