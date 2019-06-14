11307c3f7fee41f302c5a369a84ac9b247e7ab3e
Mike Slaby <mikeslaby3@gmail.com>
2019-06-13 21:06:45 -0700

    Added nodemon code to send myself email anytime someone fills out contact form

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,4 +1,13 @@
 const db = require("../models");
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'mikenodemailer@gmail.com',
+        pass: 'n0dep@ssword18'
+    }
+});
 
 module.exports = function (app) {
 
@@ -14,6 +23,26 @@ module.exports = function (app) {
             message: req.body.message,
         }).then(function (dbContact) {
             res.json(dbContact);
+        });
+        
+        let mailOptions = {
+            from: 'mikenodemailer@gmail.com',
+            to: 'mikeslaby3@gmail.com',
+            subject: "MJS Web Dev: New message from " + req.body.name,
+            text: 'New Contact',
+            html: '<p>Name: ' + req.body.name + 
+            '</p><p>Last Name: ' + req.body.last_name + 
+            '</p><p>Email: ' + req.body.email + 
+            '</p><p>Subject: ' + req.body.subject + 
+            '</p><p>Message: ' + req.body.message + '</p>'
+        };
+        
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
         });
 
     });
